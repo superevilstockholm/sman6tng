5b1d9eafb431a4dcc876bbd36b606d95b1051dd8
superevilstockholm <balilombok11@gmail.com>
2026-04-02 23:14:02 +0700

    Created role middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Middlewares
+use App\Http\Middleware\Auth\RoleMiddleware;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,26 @@ use App\Http\Controllers\Account\UserController;
 
 // Protected
 Route::middleware(['auth'])->group(function () {
-    Route::prefix('account')->name('account.')->group(function () {
-        Route::resource('users', UserController::class)->parameters([
-            'users' => 'user',
-        ]);
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        Route::middleware('role:admin')->prefix('admin')->name('admin.')->group(function () {
+            Route::get('/', function () {
+                return '';
+            })->name('index');
+            Route::prefix('account')->name('account.')->group(function () {
+                Route::resource('users', UserController::class)->parameters([
+                    'users' => 'user',
+                ]);
+            });
+        });
+        Route::middleware('role:teacher')->prefix('teacher')->name('teacher.')->group(function () {
+            Route::get('/', function () {
+                return '';
+            })->name('index');
+        });
+        Route::middleware('role:student')->prefix('student')->name('student.')->group(function () {
+            Route::get('/', function () {
+                return '';
+            })->name('index');
+        });
     });
 });
