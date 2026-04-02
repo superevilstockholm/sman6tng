94d3a77b78689ebe7b60c11bcb3d0c0fd0ae640d
superevilstockholm <balilombok11@gmail.com>
2026-04-02 20:17:54 +0700

    Remove name field from users table (created migration and updated model)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -19,7 +17,6 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
