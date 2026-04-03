59024791c401f10f82a56e498c429e6f1a867955
superevilstockholm <balilombok11@gmail.com>
2026-04-03 15:58:12 +0700

    Refactor auth controller to different files inside the auth directory and refactor the route to make concistency

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+
+class LogoutController extends Controller
+{
+    public function attempt(Request $request): RedirectResponse
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login')->with('success', 'Berhasil keluar.');
+    }
+}
